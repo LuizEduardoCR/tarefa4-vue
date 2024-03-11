19de9f3f4c8f7c14d2830c78f5e3d743087190bc
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-03-11 16:30:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+
+const listaCidades = ['São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba', 
+  'Manaus', 
+  'Recife', 
+  'Porto Alegre',
+  'Brasília',]
+</script>
+
+<template>
+  <ul>
+    <li v-for="(listaCidades, index) in listaCidades" :key="index"> {{ listaCidades }}</li>
+  </ul>
+</template>
